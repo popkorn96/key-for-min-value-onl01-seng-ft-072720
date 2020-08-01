5c6d0fb60c045e07b3baf3d0585e7b6a54fad7f2
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-01 15:06:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
 name_hash.each do |a,b|
-    lowest_key == -a
-  lowest_value == -b 
+    lowest_key == a
+  lowest_value == b 
   if lowest_value == nil || b < lowest_value
 end
 end
